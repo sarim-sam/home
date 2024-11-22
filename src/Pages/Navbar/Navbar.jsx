9a241bc2656f755aeaa32a1ba45a238cdcc7daf8
sarim <sarimslayerali786@gmail.com>
2024-11-23 01:36:24 +0500

    first commit

---
+++ src/Pages/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='font-bold text-2xl text-center bg-green-500 text-red-500'>
+        Navbar
+    </div>
+  )
+}
+
+export default Navbar
